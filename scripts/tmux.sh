109fb95814ab0878d2ee84c6373309b70bd57bf1
Miguel Alves <migueldealmeidaalves55@gmail.com>
2025-10-23 13:01:39 -0300

    update: updated the tmux installation script to install the plugin manager that i use

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -2,5 +2,6 @@ if ! command -v git >/dev/null 2>&1; then
   sudo pacman -S --noconfirm git
 fi 
 sudo pacman -S tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 rm -rf .config/tmux
 cp -r ../tmux ~/.config
