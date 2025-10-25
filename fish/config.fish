a773e4c0ca30ed2d88f25c605ac13f63799e4486
Miguel Alves <migueldealmeidaalves55@gmail.com>
2025-10-24 21:10:01 -0300

    add: added my fish config, and a basic hyprland config

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Aliases 
+
+alias vim=nvim
+alias v=nvim
+alias c=clear
+alias e=exit
+
+
+starship init fish | source
