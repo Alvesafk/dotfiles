e39f307ad83fe244f87bd7464b30a7260259c7fa
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-03-13 10:13:41 -0300

    update: replaced some of the plugins, very good config!

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,12 +16,11 @@ vim.o.undofile = true
 vim.o.incsearch = true
 
 -- vim built in package manager!!! so cooolll
+-- { src = "https://github.com//" },
 vim.pack.add({
 	{ src = "https://github.com/wnkz/monoglow.nvim" },
 	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
 	{ src = "https://github.com/folke/tokyonight.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/echasnovski/mini.files" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/Saghen/blink.cmp" },
 	{ src = "https://github.com/L3MON4D3/LuaSnip" },
@@ -30,11 +29,24 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/norcalli/nvim-colorizer.lua" },
 	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+  { src = "https://github.com/nvim-neo-tree/neo-tree.nvim" },
+  { src = "https://github.com/MunifTanjim/nui.nvim" },
+  { src = "https://github.com/antosha417/nvim-lsp-file-operations" },
+  { src = "https://github.com/folke/snacks.nvim" },
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
 })
 
 -- requiring you love
-require "mini.pick".setup()
-require "mini.files".setup()
+require "telescope".setup()
+require("neo-tree").setup({
+  close_if_last_window = false,  
+})
+
+require('lualine').setup()
 
 require("luasnip.loaders.from_vscode").lazy_load()
 require('blink.cmp').setup({
@@ -82,25 +94,6 @@ require('colorizer').setup({
 
 require("ibl").setup()
 
--- function!!!!
-local function pick_centered(picker)
-	local height = math.floor(0.6 * vim.o.lines)
-	local width = math.floor(0.6 * vim.o.columns)
-
-	require('mini.pick').builtin[picker]({}, {
-		window = {
-			config = {
-				anchor = 'NW',
-				height = height,
-				width = width,
-				row = math.floor(0.5 * (vim.o.lines - height)),
-				col = math.floor(0.5 * (vim.o.columns - width)),
-				border = 'rounded',
-			}
-		}
-	})
-end
-
 -- visual stuff
 function coloring(data)
 	local color
@@ -136,6 +129,7 @@ vim.lsp.config("lua_ls", {
 
 -- keymapsons
 local map = vim.keymap.set
+local builtin = require('telescope.builtin')
 
 map('n', '<leader>o', ':source<CR>')
 map('n', '<leader>w', ':write<CR>')
@@ -144,9 +138,9 @@ map('n', '<leader>tm', ':terminal<CR>')
 
 map('n', '<leader>lf', vim.lsp.buf.format)
 map('n', '<leader>ca', vim.lsp.buf.code_action)
-map('n', '<leader>ff', function() pick_centered('files') end)
-map('n', '<leader>fg', function() pick_centered('grep') end)
-map('n', '<leader>e', ':lua MiniFiles.open()<CR>')
+map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+map('n', '<leader>e', ':Neotree<CR>')
 map('n', '<leader>ti', ':IBLToggle<CR>')
 
 map('n', '<leader>tt', ':Coloring<CR>')
@@ -154,5 +148,5 @@ map('n', '<leader>tt', ':Coloring<CR>')
 map({ 'n', 'v', 'x' }, '<leader>y', '"+y')
 map({ 'n', 'v', 'x' }, '<leader>d', '"+d')
 
-vim.g.user_emmet_leader_key = ','
-map('i', '<C-y>,', '<plug>(emmet-expand-abbr)')
+-- vim.g.user_emmet_leader_key = ','
+-- map('i', '<C-y>,', '<plug>(emmet-expand-abbr)')
